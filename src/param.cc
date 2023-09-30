4d5f260aee73a67bf2b46c220eef6d98cadf93c5
Balisa16 <sanderantoniusbalisa16@gmail.com>
2023-09-30 23:36:13 +0700

    Remove ip and address

--- src/param.cc
+++ src/param.cc
@@ -21,18 +21,42 @@ namespace EMIRO{
 
 		if(reader.is_open())
 			reader.close();
-		
+
 		reader.open(filename);
 	    if (reader.is_open())
 			log->write_show(LogLevel::ERROR, "Failed Load JSON File");
 	    else
 	    {
-	    	enable_use = true;
+	    	Json::Value _json_data;
+		    Json::CharReaderBuilder _reader_builder;
+		    JSONCPP_STRING errs;
+		    Json::parseFromStream(_reader_builder, reader, &_json_data, &errs);
 
+		    std::vector<JSONData> data;
+		    for (const auto& item : _json_data) {
+		        CommandItem first_command = {
+		            item["commands"][0]["name"].asString(),
+		            item["commands"][0]["command"].asString()
+		        };
+		        JSONData data_json = JSONData(
+		            item["host"].asString(),
+		            item["ip"].asString(),
+		            item["pass"].asString(),
+		            first_command
+		        );
+		        for (int i = 1; i < item["commands"].size(); i++)
+		        {
+		            CommandItem cmd_item = {item["commands"][i]["name"].asString(), item["commands"][i]["command"].asString()};
+		            data_json.add_item(cmd_item);
+		        }
+		        data.push_back(data_json);
+		    }
+	    	enable_use = true;
 	    }
 	}
 
 	Param::~Param(){
-		reader.close();
+		if(reader.is_open())
+			reader.close();
 	}
 }
