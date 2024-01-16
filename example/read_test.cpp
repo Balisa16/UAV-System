331b70871b42f257f34f029e4d13c01363fd3b7f
Balisa16 <sanderantoniusbalisa16@gmail.com>
2024-01-16 19:22:32 +0700

    Update

--- example/read_test.cpp
+++ example/read_test.cpp
@@ -7,7 +7,13 @@ int main()
     JSONReader reader;
     reader = "../copter/plan.json";
     std::vector<Target> target = reader.get_data();
-    for(auto i : target)
-        std::cout << i ;
+    for (auto i : target)
+        std::cout << i;
+    Target new_data = {
+        0,
+        "Waypoint 2",
+        1.00,
+        {0.0, 2.0, 1.0, 0.0}};
+    reader += new_data;
     return 0;
 }
--- include/jsonread.hpp
+++ include/jsonread.hpp
@@ -7,20 +7,22 @@
 #include <jsoncpp/json/json.h>
 #include <enum.hpp>
 #include <iomanip>
-namespace EMIRO{
+namespace EMIRO
+{
 	typedef struct
 	{
 		int index;
 		std::string header;
 		float speed;
 		WayPoint wp;
-	}Target;
+	} Target;
 
 	class JSONReader
 	{
 	private:
 		std::string file_path = "";
 		std::vector<Target> data;
+
 	public:
 		JSONReader();
 		JSONReader(std::string path);
@@ -28,70 +30,87 @@ namespace EMIRO{
 		void get_data(std::vector<Target> &target);
 		~JSONReader();
 		void operator=(std::string path);
-		friend std::ostream& operator<<(std::ostream& os, Target target);
+		friend std::ostream &operator<<(std::ostream &os, Target target);
+		void operator+=(const Target &target);
 	};
 
-	JSONReader::JSONReader(){}
+	JSONReader::JSONReader() {}
 
 	JSONReader::JSONReader(std::string path)
 	{
 		operator=(path);
 	}
-	
-	std::vector<Target> JSONReader::get_data()
+
+	inline std::vector<Target> JSONReader::get_data()
 	{
 		return data;
 	}
 
-	void JSONReader::get_data(std::vector<Target> &target){
+	inline void JSONReader::get_data(std::vector<Target> &target)
+	{
 		target = data;
 	}
 
-	void JSONReader::operator=(std::string path){
+	inline void JSONReader::operator=(std::string path)
+	{
 		this->file_path = path;
 		std::ifstream stream_reader(file_path);
-	    if (!stream_reader.is_open()) {
-	        std::cerr << "Failed to Open JSON File." << std::endl;
-	        return;
-	    }
+		if (!stream_reader.is_open())
+		{
+			std::cerr << "Failed to Open JSON File." << std::endl;
+			return;
+		}
 
-	    Json::Value json_value;
-	    Json::CharReaderBuilder reader;
-	    JSONCPP_STRING errs;
-	    Json::parseFromStream(reader, stream_reader, &json_value, &errs);
+		Json::Value json_value;
+		Json::CharReaderBuilder reader;
+		JSONCPP_STRING errs;
+		Json::parseFromStream(reader, stream_reader, &json_value, &errs);
 
-	    int counter = 1;
-	    data.clear();
-	    for (const auto& item : json_value) {
-	        data.push_back({
-	        	counter,
-	        	item["header"].asString(),
-	        	item["speed"].asFloat(),
-	        	{
-		        	item["x"].asFloat(),
-		        	item["y"].asFloat(),
-		        	item["z"].asFloat(),
-		        	item["yaw"].asFloat()
-	        	}
-	        });
-	       	counter++;
-	    }
+		int counter = 1;
+		data.clear();
+		for (const auto &item : json_value)
+		{
+			data.push_back({counter,
+							item["header"].asString(),
+							item["speed"].asFloat(),
+							{item["x"].asFloat(),
+							 item["y"].asFloat(),
+							 item["z"].asFloat(),
+							 item["yaw"].asFloat()}});
+			counter++;
+		}
 		stream_reader.close();
 	}
 
-	
-	std::ostream &operator<<(std::ostream &os, Target target){
-		os << std::fixed << std::setprecision(2) << 
-			"Index\t: " << target.index <<
-			"\nHeader\t: " << target.header <<
-			"\nSpeed\t: " << target.speed << 
-			"\nTarget\n\tx : " << target.wp.x <<
-			"\n\ty : " << target.wp.y <<   
-			"\n\tz : " << target.wp.z << '\n' << std::defaultfloat;
+	inline void JSONReader::operator+=(const Target &target)
+	{
+		Json::Value root;
+		Json::StreamWriterBuilder builder;
+		const std::unique_ptr<Json::StreamWriter> writer(builder.newStreamWriter());
+		std::ofstream stream_writer(file_path);
+		if (!stream_writer.is_open())
+		{
+			std::cout << "Failed to Open JSON File." << std::endl;
+			return;
+		}
+
+		root["header"] = target.header;
+		root["speed"] = target.speed;
+		root["x"] = target.wp.x;
+		root["y"] = target.wp.y;
+		root["z"] = target.wp.z;
+		writer->write(root, &stream_writer);
+		stream_writer.close();
+	}
+
+	inline std::ostream &operator<<(std::ostream &os, Target target)
+	{
+		os << std::fixed << std::setprecision(2) << "Index\t: " << target.index << "\nHeader\t: " << target.header << "\nSpeed\t: " << target.speed << "\nTarget\n\tx : " << target.wp.x << "\n\ty : " << target.wp.y << "\n\tz : " << target.wp.z << '\n'
+		   << std::defaultfloat;
 		return os;
 	}
 
-	JSONReader::~JSONReader(){}
+	JSONReader::~JSONReader() {}
 }
 
 #endif
