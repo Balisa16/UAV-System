95229c7065ae836f9f084a2aef4ac01bd1cd9082
Balisa16 <sanderantoniusbalisa16@gmail.com>
2024-01-18 23:25:54 +0700

    Update

--- include/control.hpp
+++ include/control.hpp
@@ -35,9 +35,8 @@ namespace EMIRO
 
         Control(std::shared_ptr<Copter> copter, std::shared_ptr<Logger> logger, std::shared_ptr<GPS> gps);
 
-        void go(float x, float y, float z, float precision = 0.1f);
-
-        void go(float x, float y, float z, int yaw, float precision, int yaw_precision = 5);
+        void go(const float &x, const float &y, const float &z,
+                const int &yaw, const float &linear_precision = 0.2, const int &angular_precision = 5);
 
         ~Control();
     };
@@ -61,63 +60,8 @@ namespace EMIRO
             copter->set_vel(vx, vy, vz, 0.0f, 0.0f, 0.0f);
     }
 
-    void Control::go(float x, float y, float z, float precision)
-    {
-        check_init();
-        logger->write_show(LogLevel::INFO, "Go : %.2f, %.2f, %.2f", x, y, z);
-        std::cout << std::fixed << std::setprecision(2);
-
-        // Loop variable
-        ros::Rate r(5);
-        Position pos;
-        Quaternion quat;
-        Euler eul;
-
-        while (ros::ok())
-        {
-            // Get current position and orientation
-            copter->get_pose(&pos, &quat);
-            eul = to_euler(quat.w, quat.x, quat.y, quat.z);
-
-            // Close if position in target zone
-            if (std::fabs(pos.x - x) < precision &&
-                std::fabs(pos.y - y) < precision)
-                break;
-
-            float diff_x = x - pos.x;
-            float diff_y = y - pos.y;
-            float diff_z = z - pos.z;
-            vx = diff_x;
-            vy = diff_y;
-            vz = diff_z;
-            if (std::fabs(vx) > speed_limit)
-                vx = (vx > 0) ? speed_limit : -speed_limit;
-            else if (std::fabs(vx) < precision)
-                vx = 0.0f;
-
-            if (std::fabs(vy) > speed_limit)
-                vy = (vy > 0) ? speed_limit : -speed_limit;
-            else if (std::fabs(vy) < precision)
-                vy = 0.0f;
-
-            if (std::fabs(vz) > speed_limit)
-                vz = (vz > 0) ? speed_limit : -speed_limit;
-            else if (std::fabs(diff_z) < precision)
-                vz = 0.0f;
-
-            go(false);
-
-            // Print position
-            std::cout << "To target x:" << diff_x << ", y:" << diff_y << ", z:" << diff_z << "   \r";
-            std::cout.flush();
-
-            ros::spinOnce();
-            r.sleep();
-        }
-        logger->write_show(LogLevel::INFO, "Reached x:%.2f->%.2f, y:%.2f->%.2f, z:%.2f->%.2f", x, pos.x, y, pos.y, z, pos.z);
-    }
-
-    inline void Control::go(float x, float y, float z, int yaw, float precision, int yaw_precision)
+    inline void Control::go(const float &x, const float &y, const float &z,
+                            const int &yaw, const float &linear_precision, const int &angular_precision)
     {
         check_init();
         logger->write_show(LogLevel::INFO, "Go : %.2f, %.2f, %.2f, %d", x, y, z, yaw);
@@ -142,10 +86,10 @@ namespace EMIRO
             _yaw = eul.yaw;
 
             // Close if position in target zone
-            if (std::fabs(pos.x - x) < precision &&
-                std::fabs(pos.y - y) < precision &&
-                std::fabs(pos.z - z) < precision &&
-                std::fabs(eul.yaw - yaw) < yaw_precision)
+            if (std::fabs(pos.x - x) < linear_precision &&
+                std::fabs(pos.y - y) < linear_precision &&
+                std::fabs(pos.z - z) < linear_precision &&
+                std::fabs(eul.yaw - yaw) < angular_precision)
                 break;
 
             float diff_x = x - pos.x;
@@ -156,40 +100,40 @@ namespace EMIRO
             vx = diff_x;
             vy = diff_y;
             vz = diff_z;
-            avy = diff_yaw;
+            avz = diff_yaw;
             if (std::fabs(vx) > speed_limit)
                 vx = (vx > 0) ? speed_limit : -speed_limit;
-            else if (std::fabs(vx) < precision)
+            else if (std::fabs(vx) < linear_precision)
                 vx = 0.0f;
 
             if (std::fabs(vy) > speed_limit)
                 vy = (vy > 0) ? speed_limit : -speed_limit;
-            else if (std::fabs(vy) < precision)
+            else if (std::fabs(vy) < linear_precision)
                 vy = 0.0f;
 
             if (std::fabs(vz) > speed_limit)
                 vz = (vz > 0) ? speed_limit : -speed_limit;
-            else if (std::fabs(diff_z) < precision)
+            else if (std::fabs(diff_z) < linear_precision)
                 vz = 0.0f;
 
-            avy = diff_yaw * 3.14 / 180.0f;
-            if (std::fabs(avy) > rpy_speed_limit)
-                avy = (avy > 0) ? rpy_speed_limit : -rpy_speed_limit;
-            else if (std::fabs(eul.yaw - yaw) < yaw_precision)
-                avy = 0.0f;
+            avz = diff_yaw * 3.14 / 180.0f;
+            if (std::fabs(avz) > rpy_speed_limit)
+                avz = (avz > 0) ? rpy_speed_limit : -rpy_speed_limit;
+            else if (std::fabs(eul.yaw - yaw) < angular_precision)
+                avz = 0.0f;
 
             // go(true);
-            copter->set_vel(vx, vy, vz, 0.0f, 0.0f, avy);
+            copter->set_vel(vx, vy, vz, 0.0f, 0.0f, avz);
 
             // Print position
-            std::cout << C_MAGENTA << S_BOLD << "[PROGRESS] " << C_RESET << "To target x:" << diff_x << ", y:" << diff_y << ", z:" << diff_z << ", yaw:" << diff_yaw << "->" << avy << "    \r";
+            std::cout << C_MAGENTA << S_BOLD << " >>> " << C_RESET << "Target (" << diff_x << ", " << diff_y << ", " << diff_z << ", " << diff_yaw << "°)     \r";
             std::cout.flush();
 
             ros::spinOnce();
             r.sleep();
         }
-        logger->write_show(LogLevel::INFO, "Reached x:%.2f->%.2f, y:%.2f->%.2f, z:%.2f->%.2f, yaw:%.2f->%.2f",
-                           x, pos.x, y, pos.y, z, pos.z, yaw, std::fabs(_yaw - yaw));
+        logger->write_show(LogLevel::INFO, "Reached (%.2f, %.2f, %.2f, %d°) => (%.2f, %.2f, %.2f, %d°)",
+                           x, y, z, pos.x, pos.y, pos.z, (int)yaw, (int)_yaw);
     }
 
     Control::~Control()
