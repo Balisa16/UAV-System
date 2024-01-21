e4681ee58207dddfd89a14ae4e42da761113dcea
Balisa16 <sanderantoniusbalisa16@gmail.com>
2024-01-21 17:36:31 +0700

    Calculate object degree

--- example/asynccam-test.cpp
+++ example/asynccam-test.cpp
@@ -1,6 +1,20 @@
+#include <enum.hpp>
 #include <async_cam.hpp>
 
 using EMIRO::AsyncCam;
+
+static void rotate_point(Point &p, double angle)
+{
+    double cos_theta = std::cos(angle);
+    double sin_theta = std::sin(angle);
+
+    // Rotation matrix
+    double newX = p.x * cos_theta - p.y * sin_theta;
+    double newY = p.x * sin_theta + p.y * cos_theta;
+
+    p.x = newX;
+    p.y = newY;
+}
 
 int main()
 {
@@ -16,6 +30,10 @@ int main()
         cam.getobject(circles, frame);
         if (!circles.empty())
         {
+            int frame_w = frame.size().width;
+            int frame_h = frame.size().height;
+            int frame_w2 = frame_w / 2;
+            int frame_h2 = frame_h / 2;
             selected_object = circles[0];
 
             // Select largest object
@@ -39,8 +57,14 @@ int main()
             int radius = cvRound(selected_object[2]);
 
             // circle center and outline
-            cv::circle(frame, center, 3, cv::Scalar(0, 255, 0), -1, 8, 0);
+            // cv::circle(frame, center, 3, cv::Scalar(0, 255, 0), -1, 8, 0);
             cv::circle(frame, center, radius, cv::Scalar(0, 0, 255), 3, 8, 0);
+
+            // draw line from center to object
+            cv::line(frame, cv::Point(frame_w2, frame_h2), center, cv::Scalar(0, 255, 0), 3);
+            std::cout << "Degree : " << 90 - std::atan2(center.x - frame_w2, frame_h2 - center.y) * (180.0 / M_PI) << "    \r";
+            std::cout.flush();
+            // cv::line(frame, cv::Point(frame_w2, frame_h2), center, cv::Scalar(0, 255, 0), 3);
         }
         putText(frame, "ESC to exit", Point(20, 20), cv::FONT_HERSHEY_PLAIN, 1, Scalar(255, 0, 0), 2);
         cv::line(frame, cv::Point(cam.width / 2, 0), cv::Point(cam.width / 2, cam.height), cv::Scalar(255, 0, 0), 2);
