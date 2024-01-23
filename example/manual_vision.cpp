f3c28134c4e24c7ea7ace767618cbaad9cd790ca
Balisa16 <sanderantoniusbalisa16@gmail.com>
2024-01-23 15:54:50 +0700

    Update

--- example/manual_vision.cpp
+++ example/manual_vision.cpp
@@ -78,11 +78,11 @@ int main(int argc, char **argv)
                 }
             }
 
-            cv::Point center(cvRound(selected_object[0]), cvRound(selected_object[1]));
+            Point center(cvRound(selected_object[0]), cvRound(selected_object[1]));
             int radius = cvRound(selected_object[2]);
 
             // Draw object outline
-            cv::circle(frame, center, radius, cv::Scalar(0, 0, 255), 3, 8, 0);
+            circle(frame, center, radius, Scalar(0, 0, 255), 3, 8, 0);
 
             center.x -= frame_w2;
             center.y -= frame_h2;
@@ -96,19 +96,20 @@ int main(int argc, char **argv)
             AsyncCam::point_buffer(center, 10);
 
             // Draw control points
-            cv::circle(frame, center, 9, cv::Scalar(0, 255, 0), -1, 8, 0);
+            circle(frame, center, 9, Scalar(0, 255, 0), -1, 8, 0);
         }
-        cv::circle(frame, Point(frame_w2, frame_h2), control_radius, cv::Scalar(0, 0, 255), 3, 8, 0);
-        putText(frame, "ESC to exit", Point(20, 20), cv::FONT_HERSHEY_PLAIN, 1, Scalar(255, 0, 0), 2);
-        cv::line(frame, cv::Point(cam.width / 2, 0), cv::Point(cam.width / 2, cam.height), cv::Scalar(255, 0, 0), 2);
-        cv::line(frame, cv::Point(0, cam.height / 2), cv::Point(cam.width, cam.height / 2), cv::Scalar(255, 0, 0), 2);
+        circle(frame, Point(frame_w2, frame_h2), control_radius, Scalar(0, 0, 255), 3, 8, 0);
+        putText(frame, "ESC to exit", Point(20, 20), FONT_HERSHEY_PLAIN, 1, Scalar(255, 0, 0), 2);
+        line(frame, Point(cam.width / 2, 0), Point(cam.width / 2, cam.height), Scalar(255, 0, 0), 2);
+        line(frame, Point(0, cam.height / 2), Point(cam.width, cam.height / 2), Scalar(255, 0, 0), 2);
         if (frame.size().width > 0)
-            cv::imshow("Result", frame);
-        cv::waitKey(1);
+            imshow("Result", frame);
+        waitKey(1);
         if (kb.get_key() == 27)
             break;
         cam.sync_fps();
     }
+    cam.stop();
     copter->Land();
     ros::Duration(5).sleep();
     return 0;
