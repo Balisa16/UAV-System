3f34ef05bc73e218f62d57d72b6f22d632ca2ae1
Balisa16 <sanderantoniusbalisa16@gmail.com>
2024-03-18 21:10:13 +0700

    Update json

--- example/read_test.cpp
+++ example/read_test.cpp
@@ -6,12 +6,12 @@ using namespace EMIRO;
 int main()
 {
     JsonIO reader;
-    reader = COPTER_DIR + "/docs/plan.json";
+    reader = COPTER_DIR + "/docs/test.json";
     reader.optimize_distance();
     std::vector<Target> target = reader.get_data_vector();
     for (auto i : target)
         std::cout << i << std::endl;
-    Target new_data = {"Waypoint 39", 1.00, {0.0, 2.0, 1.0, 0.0}};
+    Target new_data = {"Waypoint 38", 1.00, {0.0, 2.0, 1.0, 0.0}};
     reader += new_data;
     return 0;
 }
