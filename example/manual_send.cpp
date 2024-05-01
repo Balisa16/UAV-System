f501cc30903a208b768645d00dc16f273dfc0234
Balisa16 <sanderantoniusbalisa16@gmail.com>
2024-05-01 19:51:54 +0700

    Update

--- example/manual_send.cpp
+++ example/manual_send.cpp
@@ -8,40 +8,45 @@ int main(int argc, char **argv)
 {
 
     Copter::init(argc, argv);
+    std::cout << "Running\n";
+    ros::Duration(5).sleep();
 
-    if (!Copter::PreArmedCheck())
-    {
-        Copter::Land();
-        exit(EXIT_FAILURE);
-    }
+    // if (!Copter::PreArmedCheck())
+    // {
+    //     Copter::Land();
+    //     exit(EXIT_FAILURE);
+    // }
 
-    Copter::takeoff(1);
+    // Copter::takeoff(1);
     // ros::Duration(10).sleep();
 
     // Read JSON point
-    JsonIO reader;
-    reader = COPTER_DIR + "/docs/plan.json";
-    reader.optimize_distance();
-    std::vector<Target> target = reader.get_data_vector();
+    // JsonIO reader;
+    // reader = COPTER_DIR + "/docs/plan.json";
+    // reader.optimize_distance();
+    // std::vector<Target> target = reader.get_data_vector();
 
     // Set Speed limit
-    PIDControl::get().set_rotation_speed(10.f);
-    PIDControl::get().set_linear_tolerance(0.2f);
-    PIDControl::get().set_rotation_tolerance(5.f);
-    Copter::set_yaw(YawMode::RELATIVE);
+    // PIDControl::get().set_rotation_speed(10.f);
+    // PIDControl::get().set_linear_tolerance(0.2f);
+    // PIDControl::get().set_rotation_tolerance(5.f);
+    // Copter::set_yaw(YawMode::RELATIVE);
 
-    for (Target &t : target)
+    int _cnt = 20;
+
+    while (_cnt)
     {
         if (!ros::ok())
         {
             Copter::Land();
             exit(EXIT_FAILURE);
         }
-        std::cout << C_GREEN << S_BOLD << '[' << t.header << ']' << C_RESET << '\n';
+        std::cout << "Counter : " << _cnt << '\n';
+        // std::cout << C_GREEN << S_BOLD << '[' << t.header << ']' << C_RESET << '\n';
 
-        PIDControl::get().set_target_point(t.wp);
-        PIDControl::get().set_linear_speed(t.speed);
-        PIDControl::get().go_wait(true);
+        // PIDControl::get().set_target_point(t.wp);
+        // PIDControl::get().set_linear_speed(t.speed);
+        // PIDControl::get().go_wait(true);
 
         Position pos;
         Quaternion quat;
@@ -54,8 +59,9 @@ int main(int argc, char **argv)
         client.read_response();
         client.close();
 
-        ros::Duration(1).sleep();
-        // Control::go(t.wp.x, t.wp.y, t.wp.z, t.wp.yaw, 0.05f, 5);
+        ros::Duration(5).sleep();
+        _cnt--;
+        // ros::Duration(1).sleep();
     }
 
     return 0;
